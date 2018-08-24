bf3be1719b332acf15656b5c08bcee5cec5af152
reluem <lucas.rech@me.com>
2018-08-24 15:26:28 +0200

    remove haste_plus usage for array

--- src/Resources/contao/classes/LCMitglieder.php
+++ src/Resources/contao/classes/LCMitglieder.php
@@ -1,31 +1,29 @@
 <?php
 // classes/LCMitglieder.php
-namespace reluem\LC_Mitglieder\classes;
-
-/**
- * Class LC_Mitglieder
- * @package LC_Mitglieder\classes
- */
-class LC_Mitglieder extends \ContentElement
-{
-    
-    protected $strTemplate = 'ce_LCMitglieder_list';
+    namespace reluem\LC_Mitglieder\classes;
     
     /**
-     * Compile the current element
+     * Class LC_Mitglieder
+     * @package LC_Mitglieder\classes
      */
-    protected function compile()
+    class LC_Mitglieder extends \ContentElement
     {
-        $objMember = $this->Database->execute("SELECT * FROM tl_LC_Mitglieder where published = '1' ORDER BY name_last, name_first ");
         
+        protected $strTemplate = 'ce_LCMitglieder_list';
         
-        if (($this->fe_template != $this->strTemplate) && ($this->fe_template != '')) {
-            $this->strTemplate = $this->fe_template;
-            $this->Template = new \FrontendTemplate($this->strTemplate);
+        /**
+         * Compile the current element
+         */
+        protected function compile()
+        {
+            $objMember = $this->Database->execute("SELECT * FROM tl_LC_Mitglieder where published = '1' ORDER BY name_last, name_first ");
+            
+            
+            if (($this->fe_template != $this->strTemplate) && ($this->fe_template != '')) {
+                $this->strTemplate = $this->fe_template;
+                $this->Template = new \FrontendTemplate($this->strTemplate);
+            }
+            
+            $this->Template->members = $objMember->fetchAllAssoc();
         }
-        
-        $arrObj = $objMember->fetchAllAssoc();
-        
-        $this->Template->members = \HeimrichHannot\Haste\Util\Arrays::ObjecttoArray($arrObj);
     }
-}
