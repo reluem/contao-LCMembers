6bfcb8ae0c94493c11129a28a79fce9a2e0aac52
reluem <lucas.rech@me.com>
2019-01-05 13:14:09 +0100

    change mode selection from customTPl to specific modules
    Signed-off-by: reluem <lucas.rech@me.com>

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,10 +2,6 @@
 
 // config/config.php
     
-    $GLOBALS['BE_MOD']['content']['LC_Mitglieder'] = array(
-    'tables' => array('tl_LC_Mitglieder'),
-    'icon' => 'system/modules/LC_Mitglieder/assets/images/LIONS_BE_Logo.gif',
-);
-
-$GLOBALS['TL_CTE']['LIONS']['LC_Mitglieder'] = \Reluem\LCMembers::class;
-
+    $GLOBALS['TL_CTE']['LIONS']['LCMitglieder_list'] = \Reluem\LCMembers::class;
+    $GLOBALS['TL_CTE']['LIONS']['LCMitglieder_table'] = \Reluem\LCMembers::class;
+    $GLOBALS['TL_CTE']['LIONS']['LCMitglieder_gallery'] = \Reluem\LCMembers::class;
--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,14 +1,8 @@
 <?php
     
-    $GLOBALS['TL_DCA']['tl_content']['palettes']['LC_Mitglieder'] = '{type_legend},type, headline; {text_legend}, text, customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
+    $GLOBALS['TL_DCA']['tl_content']['palettes']['LCMitglieder'] = '{type_legend},type, headline; {text_legend}, text, customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
+    $GLOBALS['TL_DCA']['tl_content']['palettes']['LCMitglieder_list'] = '{type_legend},type, headline; {text_legend}, text;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
     
-    
-    $GLOBALS['TL_DCA']['tl_content']['fields']['customTpl']['options'] = array
-    (
-        'ce_LCMitglieder_gallery',
-        'ce_LCMitglieder_list',
-        'ce_LCMitglieder_table',
-    );
     $GLOBALS['TL_DCA']['tl_content']['fields']['text']['eval'] = array
     (
         'mandatory' => false,
@@ -16,4 +10,3 @@
         'helpwizard' => true,
     );
     
-    unset($GLOBALS['TL_DCA']['tl_content']['fields']['customTpl']['options_callback']);
