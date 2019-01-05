2cb1f8032d1bd919aac5307f86210e0090e49709
reluem <lucas.rech@me.com>
2019-01-05 13:19:10 +0100

    change mode selection from customTPl to specific modules
    Signed-off-by: reluem <lucas.rech@me.com>

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,6 +1,6 @@
 <?php
     
-    $GLOBALS['TL_DCA']['tl_content']['palettes']['LCMitglieder'] = '{type_legend},type, headline; {text_legend}, text, customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
+    $GLOBALS['TL_DCA']['tl_content']['palettes']['LCMitglieder_gallery'] = '{type_legend},type, headline;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
     $GLOBALS['TL_DCA']['tl_content']['palettes']['LCMitglieder_list'] = '{type_legend},type, headline; {text_legend}, text;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
     $GLOBALS['TL_DCA']['tl_content']['palettes']['LCMitglieder_table'] = '{type_legend},type, headline; {protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
     
